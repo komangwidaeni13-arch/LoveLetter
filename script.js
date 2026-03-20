fe2157a26f6ee50011be82b641bffb5f9589721a
komangwidaeni13-arch <komangwidaeni13@gmail.com>
2026-03-20 17:34:22 +0800

    Update script.js with typewriter effect function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Typewriter effect function
 function typeWriter(text, i = 0) {
     if (i < text.length) {
-        document.getElementById('output').innerHTML += text.charAt(i);
+        document.getElementById('loveLetter').innerHTML += text.charAt(i);
         i++;
         setTimeout(() => typeWriter(text, i), 500);
     }
